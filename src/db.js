4a2f3e2082ca2764f65141b74dfa59dd6d8541dc
PavelSuchkov <suchkovp@gmail.com>
2022-06-16 16:09:13 +0300

    application ready for production 1.1

--- src/db.js
+++ src/db.js
@@ -1,11 +1,13 @@
 const mongoose = require("mongoose");
 
 module.exports = {
+
   connect: DB_HOST => {
     mongoose.set("useNewUrlParser", true);
     mongoose.set("useFindAndModify", false);
     mongoose.set("useCreateIndex", true);
     mongoose.set("useUnifiedTopology", true);
+    console.log('DB_HOST ', DB_HOST )
     mongoose.connect(DB_HOST);
 
     mongoose.connection.on("error", err => {
